b72c95732e7c1709df0ec586d365bf1f704b5b35
palomara <palomarangelll@hotmail.com>
2019-04-29 14:48:18 -0300

    challenge finished

---
+++ IMDB_charts.R
@@ -0,0 +1,48 @@
+library(RMySQL)
+library(dbConnect)
+library(ggplot2)
+con <- dbConnect(RMySQL::MySQL(),
+                 dbname = "looqbox_challenge", 
+                 host="35.199.127.241", 
+                 port = 3306, 
+                 user="looqbox-challenge", 
+                 password = "looq-challenge")
+
+dbListTables(con)
+
+tfd <- dbGetQuery(con, paste("select * from IMDB_movies"))
+
+summary(tfd)
+
+tfd_drama <- dbGetQuery(con, paste("select * from IMDB_movies where genre = 'Drama'"))
+
+tfd_comedy <- dbGetQuery(con, paste("select * from IMDB_movies where genre = 'Comedy'"))
+
+
+#Histogram for Votes x Rating
+qplot(tfd$Rating, geom = "histogram",
+      main = "Most Rated Movies", 
+      xlab = "Rate",
+      ylab = "Votes",
+      fill=I("lightblue"), 
+      col=I("black"),
+      binwidth = 0.5,
+)
+
+#Metascore x Votes
+ggplot(tfd, aes(x=Votes, y=Metascore)) + geom_point(color="green")
+
+#Comparing rating of Drama Movies
+ggplot(tfd_drama, aes(x=Title, y=Rating)) + 
+      geom_bar(stat='identity', aes(fill= Rating), width=0.5) +
+      labs(subtitle="Scale Rating of Drama Movies", 
+      title= "Drama Movies") + 
+      coord_flip()
+
+#Comparing rating of Comedy Movies
+ggplot(tfd_comedy, aes(x=Title, y=Rating)) + 
+  geom_bar(stat="identity", width=.5, fill="tomato3") + 
+  labs(title="Comedy Movies", 
+       subtitle="Scale Rating of Comedy Movies", 
+       caption="source: mpg") + 
+  theme(axis.text.x = element_text(angle=65, vjust=0.6))
--- looqbox_challenge.sql
+++ looqbox_challenge.sql
@@ -24,6 +24,30 @@ from data_product_sales as A
 inner join data_store_cad as B
 on B.store_code = A.store_code order by sales_value desc;
 
+-- data_store_sales analysis 
+select * from data_store_sales order by SALES_VALUE desc;
+
+select distinct A.store_code, B.business_name
+from data_store_sales as A
+inner join data_store_cad as B
+on A.store_code = B.store_code;
+
 -- The sale of each business area in the first quarter of 2019
 
+-- Value of the sales
+select FORMAT(sum(A.sales_value),2) "sales - R$", B.business_name 
+from data_store_sales as A
+left join data_store_cad as B
+on B.store_code = A.store_code where date between '2018-12-31 00:00:00' and '2019-03-31 23:59:00' 
+group by business_name;
+
+-- Quantity of the sales
+select sum(A.sales_qty) "qty" , B.business_name
+from data_store_sales as A
+left join data_store_cad as B
+on B.store_code = A.store_code 
+where date between '2018-12-31 00:00:00' and '2019-03-31 23:59:00' 
+group by business_name;
+
 -- github/palomara - looqbox_challenge
+
